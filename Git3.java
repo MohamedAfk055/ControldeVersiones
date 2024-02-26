5ff1c587aa83be1cef33a401e7a341d316d44636
Mohamed Achouragh Afkir <machouragh@iesgpb.net>
2024-02-26 18:31:31 +0100

    Version1.6

--- Git3.java
+++ Git3.java
@@ -7,6 +7,19 @@ public class Git3 {
 		int c = a*b;
 
 	}
+	public void div() {
+		int a=2142;
+		int b=1122;
+		int c = a/b*12;
+		
 
+	}
+	public void autoincrement() {
+		int a=1;
+		int b=0;
+		int c = b+a;
+		
+
+	}
 	
 }
